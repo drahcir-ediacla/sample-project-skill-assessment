f489a4996172e72ed316cff2c5b3c716d1f29a38
Richard Alcaide <richardalcaide16@gmail.com>
2025-01-20 12:12:09 +0800

    lazy commit

--- app/admin/ArticleList.tsx
+++ app/admin/ArticleList.tsx
@@ -6,7 +6,6 @@ import Button from "../components/Button"
 import styles from "./page.module.css"
 import Link from "next/link"
 import axiosHandler from "../utils/axiosHandler"
-import Image from "next/image";
 
 interface Company {
   id: number;
@@ -115,7 +114,7 @@ const ArticleList = () => {
                     <button className={styles.editBtn}>Edit</button>
                   }
                 </td>
-                <td><Image src={articleItem.image} alt="" /></td>
+                <td><img src={articleItem.image} alt="" /></td>
                 <td>{articleItem.title}</td>
                 <td><Link href={articleItem.link} target="_blank">View Link</Link></td>
                 <td>{articleItem.writer.firstname} {articleItem.writer.lastname}</td>
